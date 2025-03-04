f1f6e18e10f063188972ee467df34ae47fa21739
germanhzmra <4071027@alu.murciaeduca.es>
2025-03-04 14:26:03 +0100

    Add files via upload

--- creditos.gd
+++ creditos.gd
@@ -2,5 +2,8 @@ extends Control
 
 
 
+
+
+
 func _on_salir_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
--- menu.gd
+++ menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://juego.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _on_instrucciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://instrucciones.tscn")
+	
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://creditos.tscn")
+	
